ee55146473b1962e7b99fe6f412f25795e891ad1
Kacper Faber <kacperf1234@gmail.com>
2021-10-19 17:40:30 +0200

    Created 'ReportStorage', it has method to get report by date.

--- src/main/kotlin/io/github/kacperfaber/analyzers/Analyzer.kt
+++ src/main/kotlin/io/github/kacperfaber/analyzers/Analyzer.kt
@@ -1,7 +1,8 @@
 package io.github.kacperfaber.analyzers
 
 import io.github.kacperfaber.reports.Report
+import io.github.kacperfaber.reports.ReportStorage
 
 interface Analyzer {
-    fun analyze(todayReport: Report, reports: Array<Report>): AnalyzerResult
+    fun analyze(todayReport: Report, reports: ReportStorage): AnalyzerResult
 }
---
+++ src/main/kotlin/io/github/kacperfaber/reports/ReportStorage.kt
@@ -0,0 +1,9 @@
+package io.github.kacperfaber.reports
+
+import java.time.LocalDate
+
+class ReportStorage(var reports: Array<Report>) {
+    fun getReport(date: LocalDate): Report? {
+        return reports.firstOrNull { x -> x.reportDate!!.toLocalDate() == date }
+    }
+}
