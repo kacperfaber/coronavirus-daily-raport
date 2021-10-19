78ef597260d50f0f214b0a58c947459f18bcfc3f
Kacper Faber <kacperf1234@gmail.com>
2021-10-19 18:03:10 +0200

    Added NewDeathsAnalyzer

--- src/main/kotlin/io/github/kacperfaber/analyzers/Labels.kt
+++ src/main/kotlin/io/github/kacperfaber/analyzers/Labels.kt
@@ -2,6 +2,7 @@ package io.github.kacperfaber.analyzers
 
 class Labels {
     companion object {
-        var newInfections = "New Infections"
+        const val newDeaths = "New Deaths"
+        const val newInfections = "New Infections"
     }
 }
