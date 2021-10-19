576e441f3718409a239720301553f3e1e8494a7e
Kacper Faber <kacperf1234@gmail.com>
2021-10-19 17:20:56 +0200

    Deleted ValueLabel.kt

--- src/main/kotlin/io/github/kacperfaber/analyzers/ValueLabel.kt
+++
@@ -1,5 +0,0 @@
-package io.github.kacperfaber.analyzers
-
-import java.time.LocalDate
-
-data class ValueLabel(val value: Long, val name: String, val change: Long, val changedBy: LocalDate) : Element()
