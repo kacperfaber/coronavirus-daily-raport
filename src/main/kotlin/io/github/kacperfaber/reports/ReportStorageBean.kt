6e30e32ef15bbfb3edcbb544b3ea30c861d9cf70
Kacper Faber <kacperf1234@gmail.com>
2021-10-20 14:31:54 +0200

    Created ReportStorageBean

---
+++ src/main/kotlin/io/github/kacperfaber/reports/ReportStorageBean.kt
@@ -0,0 +1,12 @@
+package io.github.kacperfaber.reports
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+open class ReportStorageBean {
+    @Bean
+    open fun reportStorage(api: ApiService): ReportStorage {
+        return ReportStorage(api.getAllReports())
+    }
+}
