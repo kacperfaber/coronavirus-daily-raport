f9216450b0ec40c5ef1e4a018e8e7caa0841c5a4
Kacper Faber <kacperf1234@gmail.com>
2021-10-20 16:48:59 +0200

    Created MailConfiguration and bean

---
+++ src/main/kotlin/io/github/kacperfaber/emails/MailConfiguration.kt
@@ -0,0 +1,3 @@
+package io.github.kacperfaber.emails
+
+data class MailConfiguration(var from: String)
---
+++ src/main/kotlin/io/github/kacperfaber/emails/MailConfigurationBean.kt
@@ -0,0 +1,16 @@
+package io.github.kacperfaber.emails
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+open class MailConfigurationBean {
+    @Value("mail.from")
+    lateinit var from: String
+
+    @Bean
+    open fun mailConfiguration(): MailConfiguration {
+        return MailConfiguration(from)
+    }
+}
