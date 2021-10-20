ab494b7d3f00ca9c4a2160dcc49ed0bbf0e1b5b1
Kacper Faber <kacperf1234@gmail.com>
2021-10-20 13:26:45 +0200

    Code styles fixes

--- src/main/kotlin/io/github/kacperfaber/thymeleaf/TemplateEngineBean.kt
+++ src/main/kotlin/io/github/kacperfaber/thymeleaf/TemplateEngineBean.kt
@@ -1,7 +1,6 @@
-package io.github.kacperfaber.emails
+package io.github.kacperfaber.thymeleaf
 
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.*
 import org.thymeleaf.TemplateEngine
 import org.thymeleaf.templateresolver.ITemplateResolver
 
