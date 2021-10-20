486dd617acb39a9df5cc668476d65d29a42d6d24
Kacper Faber <kacperf1234@gmail.com>
2021-10-20 14:44:46 +0200

    Optymalized ApiService.kt

--- src/main/kotlin/io/github/kacperfaber/reports/ApiService.kt
+++ src/main/kotlin/io/github/kacperfaber/reports/ApiService.kt
@@ -1,23 +1,10 @@
 package io.github.kacperfaber.reports
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 
 @Service
-class ApiService {
-    @Autowired
-    lateinit var reportJsonReader: JsonReader<Report>
-
-    @Autowired
-    lateinit var httpService: HttpService
-
-    @Autowired
-    lateinit var dateWriter: IDateTimeWriter
-
-    @Autowired
-    lateinit var reportsJsonReader: JsonReader<Array<Report>>
-
+class ApiService(val reportJsonReader: JsonReader<Report>, var httpService: HttpService, var dateWriter: IDateTimeWriter, var reportsJsonReader: JsonReader<Array<Report>>) {
     fun getReport(): Report {
         val content = httpService.getContent("https://koronawirus-api.herokuapp.com/api/covid-vaccinations-tests/daily")
         return reportJsonReader.read(content)
