a1b6031dc87af4b4d499f60c420146f8cff593c4
Kacper Faber <kacperf1234@gmail.com>
2021-10-20 20:07:18 +0200

    Fixed issue in DefaultDateTimeWriter_writeDate.kt

--- src/test/kotlin/io/github/kacperfaber/reports/tests/DefaultDateTimeWriter_writeDate.kt
+++ src/test/kotlin/io/github/kacperfaber/reports/tests/DefaultDateTimeWriter_writeDate.kt
@@ -3,27 +3,28 @@ package io.github.kacperfaber.reports.tests
 import io.github.kacperfaber.reports.DefaultDateTimeWriter
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import java.time.LocalDate
 import java.time.LocalDateTime
 import kotlin.random.Random
 
 class DefaultDateTimeWriter_writeDate {
-    fun exec(l: LocalDateTime): String {
+    fun exec(l: LocalDate): String {
         return DefaultDateTimeWriter().writeDate(l)
     }
     @Test
     fun doesNotThrow() {
-        Assertions.assertDoesNotThrow { exec(LocalDateTime.of(2021, 10, 18, 12, 30, 30)) }
+        Assertions.assertDoesNotThrow { exec(LocalDate.of(2021, 10, 18)) }
     }
     @Test
     fun startsWithYear() {
         val year = Random.nextInt(2019, 2030)
-        val res = exec(LocalDateTime.of(year, 10, 18, 12, 30, 30))
+        val res = exec(LocalDate.of(year, 10, 18))
         Assertions.assertTrue(res.startsWith(year.toString()))
     }
     @Test
     fun endsWithDay() {
         val day = Random.nextInt(0,12)
-        val res = exec(LocalDateTime.of(2021, 10, day, 12, 30, 30))
+        val res = exec(LocalDate.of(2021, 10, day))
         Assertions.assertTrue(res.endsWith(day.toString()))
     }
     @Test
@@ -31,7 +32,7 @@ class DefaultDateTimeWriter_writeDate {
         val day = Random.nextInt(1, 28)
         val month = Random.nextInt(12)
         val year = Random.nextInt(2019, 2030)
-        val res = exec(LocalDateTime.of(year, month, day, 12, 30, 30))
+        val res = exec(LocalDate.of(year, month, day))
         Assertions.assertEquals("${year}-${month}-${day}", res)
     }
 }
