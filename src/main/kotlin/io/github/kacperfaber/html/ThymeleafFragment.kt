407b3bde56ed0e2e4717cd2429e83f8a5e9f131f
Kacper Faber <kacperf1234@gmail.com>
2021-12-02 11:34:31 +0100

    Now 'ThymeleafFragment -> render()' render parameters.

--- src/main/kotlin/io/github/kacperfaber/html/ThymeleafFragment.kt
+++ src/main/kotlin/io/github/kacperfaber/html/ThymeleafFragment.kt
@@ -1,14 +1,18 @@
 package io.github.kacperfaber.html
 
 class ThymeleafFragment(var fragmentDoc: String, var fragment: String, var params: HashMap<String, Any>) : EmptyHtmlElement() {
-    override fun render(): String = "<div th:insert=\"$fragmentDoc :: ${fragment}\"></div>"
+    override fun render(): String = "<div th:insert=\"$fragmentDoc :: ${renderFragmentInvocation()}\"></div>"
 
-    fun renderFragmentInvocation() = "${fragment}()"
+    fun renderFragmentInvocation() = "${fragment}(${renderParameters()})"
 
-    fun renderParameters() {
+    fun renderParameters(): String {
         val b = StringBuilder()
         for (key in params.keys) {
             b.append("${key}='${params[key]}'")
+            if (params.keys.last() != key) {
+                b.append(", ")
+            }
         }
+        return b.toString()
     }
 }
