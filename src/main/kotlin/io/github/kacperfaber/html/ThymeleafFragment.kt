3d9fd978a7a7abf8334aa949000d320143f88133
Kacper Faber <kacperf1234@gmail.com>
2021-12-02 11:11:52 +0100

    I have problem with fragments, because in interactive thymeleaf editor fragment definition is also rendering as well.
    Split it into two files should fix the problem, but yesterday I didn't get that issue.

--- src/main/kotlin/io/github/kacperfaber/html/ThymeleafFragment.kt
+++ src/main/kotlin/io/github/kacperfaber/html/ThymeleafFragment.kt
@@ -1,5 +1,14 @@
 package io.github.kacperfaber.html
 
-class ThymeleafFragment(var fragmentDoc: String, var fragment: String) : EmptyHtmlElement() {
+class ThymeleafFragment(var fragmentDoc: String, var fragment: String, var params: HashMap<String, Any>) : EmptyHtmlElement() {
     override fun render(): String = "<div th:insert=\"$fragmentDoc :: ${fragment}\"></div>"
+
+    fun renderFragmentInvocation() = "${fragment}()"
+
+    fun renderParameters() {
+        val b = StringBuilder()
+        for (key in params.keys) {
+            b.append("${key}='${params[key]}'")
+        }
+    }
 }
