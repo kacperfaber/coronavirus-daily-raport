a0adb3267488da1cd24676e21bcd7aa34583d49f
Kacper Faber <kacperf1234@gmail.com>
2021-12-03 12:48:48 +0100

    HtmlStaticEmailContentGenerator.kt is repaired, before that he was generating thymeleaf fragments (html was not processed by thymeleaf)

--- src/main/kotlin/io/github/kacperfaber/html/HtmlStaticEmailContentGenerator.kt
+++ src/main/kotlin/io/github/kacperfaber/html/HtmlStaticEmailContentGenerator.kt
@@ -1,11 +1,21 @@
 package io.github.kacperfaber.html
 
+import io.github.kacperfaber.ThymeleafContextGenerator
 import io.github.kacperfaber.emails.StaticEmailContent
 import io.github.kacperfaber.emails.StaticEmailContentGenerator
 import io.github.kacperfaber.reports.Report
+import io.github.kacperfaber.thymeleaf.ThymeleafProcessor
 import org.springframework.stereotype.Component
 
 @Component
-class HtmlStaticEmailContentGenerator(var htmlWriter: HtmlWriter) : StaticEmailContentGenerator{
-    override fun generate(today: Report): StaticEmailContent = StaticEmailContent(htmlWriter.write(today))
+class HtmlStaticEmailContentGenerator(
+    var htmlWriter: HtmlWriter,
+    var thymeleafProcessor: ThymeleafProcessor,
+    var thymeleafContextGenerator: ThymeleafContextGenerator) : StaticEmailContentGenerator {
+    override fun generate(today: Report): StaticEmailContent {
+        val thymeleafHtml = htmlWriter.write(today)
+        val thymeleafContext = thymeleafContextGenerator.generate(today)
+        val html = thymeleafProcessor.processToHtml(thymeleafHtml, thymeleafContext)
+        return StaticEmailContent(html)
+    }
 }
