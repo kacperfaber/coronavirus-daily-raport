06ab6ff159358da5d296b2e3421f734eb52d07c9
Kacper Faber <kacperf1234@gmail.com>
2021-12-03 12:14:16 +0100

    Created new ThymeleafEmailWriter.kt (modified old)

--- src/main/kotlin/io/github/kacperfaber/thymeleaf/ThymeleafEmailWriter.kt
+++ src/main/kotlin/io/github/kacperfaber/thymeleaf/ThymeleafEmailWriter.kt
@@ -1,6 +1,8 @@
 package io.github.kacperfaber.thymeleaf
 
+import io.github.kacperfaber.api.Subscription
 import io.github.kacperfaber.emails.EmailWriter
+import io.github.kacperfaber.emails.StaticEmailContent
 import io.github.kacperfaber.reports.Report
 import io.github.kacperfaber.reports.ReportStorage
 import org.springframework.stereotype.Component
@@ -8,13 +10,9 @@ import org.thymeleaf.TemplateEngine
 import org.thymeleaf.context.Context
 
 @Component
-class ThymeleafEmailWriter(var reportStorage: ReportStorage, var engine: TemplateEngine) : EmailWriter {
-    override fun write(report: Report): String {
-        val positiveTests = ((report.today!!.tests!!.infections.toDouble() / report.today!!.tests!!.tests!!.positive)).toString().take(5)
-        val infections = report.today!!.infections ?: throw NullPointerException()
-        val tests = report.today!!.tests!!.tests ?: throw NullPointerException()
-        val model = ThymeleafModel(infections.newInfections as Int, infections.newDeaths as Int, positiveTests, tests.all, tests.positive)
-        val ctx = Context().apply { setVariable("model", model) }
-        return engine.process("base-template", ctx)
+class ThymeleafEmailWriter(var engine: TemplateEngine, var emailContextGenerator: EmailContextGenerator) : EmailWriter {
+    override fun write(subscription: Subscription, content: StaticEmailContent): String {
+        val context = emailContextGenerator.generate(subscription, content.html)
+        return engine.process("email", context)
     }
 }
