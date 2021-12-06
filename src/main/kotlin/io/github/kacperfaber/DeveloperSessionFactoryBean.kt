2a92df30d515e55ddb09cd95f9a2061a8e27614e
Kacper Faber <kacperf1234@gmail.com>
2021-12-06 14:31:52 +0100

    Renamed "hibernate.cfg.xml" to "dev.hibernate.cfg.xml", cause It's used only in 'DeveloperSessionFactoryBean.kt'.
    Created DeveloperSessionFactoryBean.kt, it requires "dev" profile active.

--- src/main/kotlin/io/github/kacperfaber/DeveloperSessionFactoryBean.kt
+++ src/main/kotlin/io/github/kacperfaber/DeveloperSessionFactoryBean.kt
@@ -3,11 +3,13 @@ package io.github.kacperfaber
 import org.hibernate.SessionFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 
 @Configuration
-open class SessionFactoryBean {
+@Profile("dev")
+open class DeveloperSessionFactoryBean {
     @Bean
     open fun sessionFactory(): SessionFactory {
-        return org.hibernate.cfg.Configuration().configure("hibernate.cfg.xml").buildSessionFactory()
+        return org.hibernate.cfg.Configuration().configure("dev.dev.hibernate.cfg.xml").buildSessionFactory()
     }
 }
