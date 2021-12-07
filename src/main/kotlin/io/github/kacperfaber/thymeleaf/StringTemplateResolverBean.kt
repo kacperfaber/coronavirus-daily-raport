8a685f7e78f8d1db4e0d27135e12cbe67a0fce30
Kacper Faber <kacperf1234@gmail.com>
2021-12-07 13:09:15 +0100

    Created StringTemplateResolverBean

---
+++ src/main/kotlin/io/github/kacperfaber/thymeleaf/StringTemplateResolverBean.kt
@@ -0,0 +1,16 @@
+package io.github.kacperfaber.thymeleaf
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.thymeleaf.templatemode.TemplateMode
+import org.thymeleaf.templateresolver.StringTemplateResolver
+
+@Configuration
+open class StringTemplateResolverBean {
+    @Bean
+    open fun stringTemplateResolver(): StringTemplateResolver{
+        return StringTemplateResolver().apply {
+            templateMode = TemplateMode.HTML
+        }
+    }
+}
