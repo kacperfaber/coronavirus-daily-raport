ee2166388851cfe47ef5bb3d86aa21c25704b2f3
Kacper Faber <kacperf1234@gmail.com>
2021-12-07 15:16:48 +0100

    Added PositiveTestsElementGenerator...
    it's processing another kinda data.
    
    positive tests are not the same that confirmed cases they're.
    
    Samples:
    positive tests / all tests: 22.554%
    cases / all tests         : 20.382%

---
+++ src/main/kotlin/io/github/kacperfaber/html/PositiveTestsElementGenerator.kt
@@ -0,0 +1,15 @@
+package io.github.kacperfaber.html
+
+import io.github.kacperfaber.reports.Report
+import io.github.kacperfaber.round
+import org.springframework.stereotype.Component
+
+@Component
+class PositiveTestsElementGenerator : ElementGenerator{
+    override fun generate(todayReport: Report): EmptyHtmlElement {
+        val positive = todayReport.today!!.tests!!.tests!!.positive
+        val all = todayReport.today!!.tests!!.tests!!.all
+        val percent = ((positive * 100.0) / all).round(3)
+        return PercentThymeleafFragment("Positive Tests", positive, all, percent)
+    }
+}
