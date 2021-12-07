42d45e74fa929ccf3234b2040e860e4eb25c818a
Kacper Faber <kacperf1234@gmail.com>
2021-12-07 13:10:51 +0100

    TemplateEngineBean.kt now it's adding only ResourceAndStringTemplateResolver (from bean)

--- src/main/kotlin/io/github/kacperfaber/thymeleaf/TemplateEngineBean.kt
+++ src/main/kotlin/io/github/kacperfaber/thymeleaf/TemplateEngineBean.kt
@@ -1,15 +1,19 @@
 package io.github.kacperfaber.thymeleaf
 
-import org.springframework.context.annotation.*
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.thymeleaf.TemplateEngine
-import org.thymeleaf.templateresolver.ITemplateResolver
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
+import org.thymeleaf.templateresolver.StringTemplateResolver
 
 @Configuration
 open class TemplateEngineBean {
     @Bean
-    open fun templateEngine(resolverBean: ITemplateResolver): TemplateEngine {
+    open fun templateEngine(stringTemplateResolver: StringTemplateResolver, classLoaderTemplateResolver: ClassLoaderTemplateResolver, customTemplateResolver: ResourceAndStringTemplateResolver): TemplateEngine {
         val engine = TemplateEngine()
-        engine.setTemplateResolver(resolverBean)
+//        engine.addTemplateResolver(stringTemplateResolver)
+//        engine.addTemplateResolver(classLoaderTemplateResolver)
+        engine.addTemplateResolver(customTemplateResolver)
         return engine
     }
 }
