ec969a0b3ea2523c06c5ec1090b46a9281d483a1
Kacper Faber <kacperf1234@gmail.com>
2021-12-07 13:19:22 +0100

    WARNING! ApiService.kt, urls changed.
    getAllReports() returns 'infections' and 'tests' null.
    
    I have to change it, to another version of Report.

--- src/main/kotlin/io/github/kacperfaber/reports/ApiService.kt
+++ src/main/kotlin/io/github/kacperfaber/reports/ApiService.kt
@@ -2,10 +2,14 @@ package io.github.kacperfaber.reports
 
 import org.springframework.stereotype.Service
 import java.time.LocalDate
-import java.time.LocalDateTime
 
 @Service
-class ApiService(val reportJsonReader: JsonReader<Report>, var httpService: HttpService, var dateWriter: IDateTimeWriter, var reportsJsonReader: JsonReader<Array<Report>>) {
+class ApiService(
+    val reportJsonReader: JsonReader<Report>,
+    var httpService: HttpService,
+    var dateWriter: IDateTimeWriter,
+    var reportsJsonReader: JsonReader<Array<Report>>
+) {
     fun getReport(): Report? {
         val response = httpService.get("https://koronawirus-api.herokuapp.com/api/covid-vaccinations-tests/daily")
         if (response.isSuccessful) {
@@ -16,22 +20,36 @@ class ApiService(val reportJsonReader: JsonReader<Report>, var httpService: Http
 
     fun getReports(from: LocalDate, to: LocalDate): Array<Report> {
         val content = httpService.getContent(
-            "https://koronawirus-api.herokuapp.com/api/covid19/from/${dateWriter.writeDate(from)}/to/${dateWriter.writeDate(to)}"
+            "https://koronawirus-api.herokuapp.com/api/covid-vaccinations-tests/from/${dateWriter.writeDate(from)}/to/${
+                dateWriter.writeDate(
+                    to
+                )
+            }"
         )
         return reportsJsonReader.read(content)
     }
 
     fun getAllReports(): Array<Report> {
-        val content = httpService.getContent("https://koronawirus-api.herokuapp.com/api/covid19/from/2020-03-01/to/${dateWriter.writeDate(LocalDate.now())}")
+        val content = httpService.getContent(
+            "https://koronawirus-api.herokuapp.com/api/covid19/from/${
+                dateWriter.writeDate(
+                    LocalDate.of(2019, 12, 1)
+                )
+            }/to/${dateWriter.writeDate(LocalDate.now())}"
+        )
         return reportsJsonReader.read(content)
     }
 
     fun getReport(date: LocalDate): Report? {
-        val r = httpService.get("https://koronawirus-api.herokuapp.com/api/covid19/from/${dateWriter.writeDate(date)}/to/${dateWriter.writeDate(date.plusDays(1))}")
+        val r = httpService.get(
+            "https://koronawirus-api.herokuapp.com/api/covid-vaccinations-tests/from/${dateWriter.writeDate(date)}/to/${
+                dateWriter.writeDate(date.plusDays(1))
+            }"
+        )
         if (r.isSuccessful) {
             val content = r.body!!.string()
             val reports = reportsJsonReader.read(content)
-            return reports.find { x -> x.reportDate == date}
+            return reports.find { x -> x.reportDate == date }
         }
         return null
     }
