67b2d7cb34455c9da5b98ec78539008c7195b902
Kacper Faber <kacperf1234@gmail.com>
2021-12-07 11:24:15 +0100

    Created NewDeathsElementGenerator.

---
+++ src/main/kotlin/io/github/kacperfaber/html/NewDeathsElementGenerator.kt
@@ -0,0 +1,12 @@
+package io.github.kacperfaber.html
+
+import io.github.kacperfaber.reports.Report
+import org.springframework.stereotype.Component
+
+@Component
+class NewDeathsElementGenerator : ElementGenerator{
+    override fun generate(todayReport: Report): EmptyHtmlElement {
+        val newDeaths = todayReport.today!!.infections!!.newDeaths ?: throw NullPointerException()
+        return AddThyemelafFragment("New Deaths", newDeaths)
+    }
+}
