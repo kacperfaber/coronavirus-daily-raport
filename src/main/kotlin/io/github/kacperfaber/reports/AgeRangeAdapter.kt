f33ff71dd80aadf7eda25cb3b98cd6a4a66dd468
Kacper Faber <kacperf1234@gmail.com>
2021-12-16 16:21:33 +0100

    Changed AgeTypeAdapter.kt to AgeRangeAdapter.kt

--- src/main/kotlin/io/github/kacperfaber/reports/AgeRangeAdapter.kt
+++ src/main/kotlin/io/github/kacperfaber/reports/AgeRangeAdapter.kt
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component
 import java.lang.reflect.Type
 
 @Component
-class AgeTypeAdapter : JsonDeserializer<AgeType> {
-    override fun deserialize(p0: JsonElement?, p1: Type?, p2: JsonDeserializationContext?): AgeType {
-
+class AgeRangeAdapter : JsonDeserializer<AgeRange> {
+    override fun deserialize(p0: JsonElement?, p1: Type?, p2: JsonDeserializationContext?): AgeRange{
+        return AgeRange.valueOf(p0!!.asString)
     }
 }
