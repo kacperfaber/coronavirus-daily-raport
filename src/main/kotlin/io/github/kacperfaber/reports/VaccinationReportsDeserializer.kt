d65cf2c8a6c10afb32b7eb158e4a1fcbce2626a7
Kacper Faber <kacperf1234@gmail.com>
2021-12-16 13:45:42 +0100

    Created VaccinationReportsDeserializer

---
+++ src/main/kotlin/io/github/kacperfaber/reports/VaccinationReportsDeserializer.kt
@@ -0,0 +1,11 @@
+package io.github.kacperfaber.reports
+
+import com.google.gson.Gson
+import org.springframework.stereotype.Component
+
+@Component
+class VaccinationReportsDeserializer(var gson: Gson) : JsonReader<Array<VaccinationReport>>{
+    override fun read(str: String): Array<VaccinationReport> {
+        return gson.fromJson(str, Array<VaccinationReport>::class.java)
+    }
+}
