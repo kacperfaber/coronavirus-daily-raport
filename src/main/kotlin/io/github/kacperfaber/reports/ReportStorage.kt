6c684b5f3ccd6b759308df9214845a5c6388932f
Kacper Faber <kacperf1234@gmail.com>
2021-12-16 14:03:58 +0100

    Created 'ReportStorage -> getVaccinationReports(from, to):List<VaccinationReport>'

--- src/main/kotlin/io/github/kacperfaber/reports/ReportStorage.kt
+++ src/main/kotlin/io/github/kacperfaber/reports/ReportStorage.kt
@@ -10,4 +10,8 @@ class ReportStorage(var reports: Array<Report>, var vaccinationReports: Array<Va
     fun getVaccinationReport(date: LocalDate): VaccinationReport? {
         return vaccinationReports.firstOrNull {x -> x.reportDate == date}
     }
+
+    fun getVaccinationReports(from: LocalDate, to: LocalDate): List<VaccinationReport> {
+        return vaccinationReports.filter { x -> x.reportDate > from && x.reportDate < to }
+    }
 }
