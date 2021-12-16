2ff031eafb7cdbf1bee9ca3eb96aa903e50d1996
Kacper Faber <kacperf1234@gmail.com>
2021-12-16 13:59:18 +0100

    Added 'reportDate' to VaccinationReport.kt

--- src/main/kotlin/io/github/kacperfaber/reports/VaccinationReport.kt
+++ src/main/kotlin/io/github/kacperfaber/reports/VaccinationReport.kt
@@ -1,6 +1,9 @@
 package io.github.kacperfaber.reports
 
+import java.time.LocalDate
+
 class VaccinationReport {
+    lateinit var reportDate: LocalDate
     lateinit var general: GeneralVaccinationReport
     lateinit var today: TodayVaccinationReport
 }
