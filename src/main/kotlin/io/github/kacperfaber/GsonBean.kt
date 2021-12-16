d10c79289929c0a881f1957ded5056886bed680a
Kacper Faber <kacperf1234@gmail.com>
2021-12-16 16:21:46 +0100

    Registered AgeRangeAdapter in GsonBean

--- src/main/kotlin/io/github/kacperfaber/GsonBean.kt
+++ src/main/kotlin/io/github/kacperfaber/GsonBean.kt
@@ -2,18 +2,21 @@ package io.github.kacperfaber
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import io.github.kacperfaber.reports.AgeRange
+import io.github.kacperfaber.reports.AgeRangeAdapter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.time.LocalDate
 import java.time.LocalDateTime
 
 @Configuration
-open class GsonBean(var adapter: LocalDateTimeAdapter, var localDateAdapter: LocalDateAdapter) {
+open class GsonBean(var adapter: LocalDateTimeAdapter, var localDateAdapter: LocalDateAdapter, var ageRangeAdapter: AgeRangeAdapter) {
     @Bean
     open fun gson(): Gson {
         return GsonBuilder()
             .registerTypeAdapter(LocalDateTime::class.java, adapter)
             .registerTypeAdapter(LocalDate::class.java, localDateAdapter)
+            .registerTypeAdapter(AgeRange::class.java, ageRangeAdapter)
             .create()
     }
 }
