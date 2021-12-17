1e16938e16009ad604e8113e07938e0cd7d89533
Kacper Faber <kacperf1234@gmail.com>
2021-12-17 14:19:02 +0100

    ChartService parameters where we were thinking about data (were Any) now they're Number type.

--- src/main/kotlin/io/github/kacperfaber/quickchart/ChartService.kt
+++ src/main/kotlin/io/github/kacperfaber/quickchart/ChartService.kt
@@ -4,17 +4,17 @@ import org.springframework.stereotype.Component
 
 @Component
 class ChartService(var api: ChartApi, var payloadGenerator: PayloadGenerator) {
-    fun createDoughnut(map: HashMap<String, Any>): ChartUrl? {
+    fun createDoughnut(map: HashMap<String, Number>): ChartUrl? {
         val payload = payloadGenerator.generate(ChartType.Doughnut, map.map { x -> x.key }, listOf(map.map { x -> x.value }))
         return api.create(payload).toNullableUrl()
     }
 
-    fun createDoughnut(labels: List<String>, dataSet: List<Any>): ChartUrl?{
+    fun createDoughnut(labels: List<String>, dataSet: List<Number>): ChartUrl?{
         val payload = payloadGenerator.generate(ChartType.Doughnut, labels, listOf(dataSet))
         return api.create(payload).toNullableUrl()
     }
 
-    fun create(chartType: ChartType, labels: List<String>, dataSets: List<List<Any>>): ChartUrl? {
+    fun create(chartType: ChartType, labels: List<String>, dataSets: List<List<Number>>): ChartUrl? {
         val payload = payloadGenerator.generate(chartType, labels, dataSets)
         return api.create(payload).toNullableUrl()
     }
