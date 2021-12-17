8a6a6d77ba3470d80c3e403e98008d05592c306c
Kacper Faber <kacperf1234@gmail.com>
2021-12-17 13:30:23 +0100

    Added Component annotation to ChartApi

--- src/main/kotlin/io/github/kacperfaber/quickchart/ChartApi.kt
+++ src/main/kotlin/io/github/kacperfaber/quickchart/ChartApi.kt
@@ -4,7 +4,9 @@ import io.github.kacperfaber.JsonRequestBodyGenerator
 import io.github.kacperfaber.ResponseBodyStringReader
 import io.github.kacperfaber.reports.HttpService
 import io.github.kacperfaber.reports.JsonReader
+import org.springframework.stereotype.Component
 
+@Component
 class ChartApi(var http: HttpService, var createUrlGenerator: CreateUrlGenerator, var jsonRequestBodyGenerator: JsonRequestBodyGenerator, var payloadJsonWriter: PayloadJsonWriter, var bodyStringReader: ResponseBodyStringReader, var createReader: JsonReader<CreateResponse>){
     fun create(p: Payload): CreateResponse? {
         val url = createUrlGenerator.generate()
