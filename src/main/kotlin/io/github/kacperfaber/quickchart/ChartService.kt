764438b2bbd7c3ee3903403bad8e9acc8b864a9c
Kacper Faber <kacperf1234@gmail.com>
2021-12-17 13:42:25 +0100

    Created ChartService with three methods:
    - createDoughnut(map: HashMap<String, Any>): ChartUrl?
    - createDoughbut(labels: List<String>, dataSet: List<Any>): ChartUrl?
    - create(chartType: ChartType, labels: List<String>, dataSets: List<List<Any>>)

---
+++ src/main/kotlin/io/github/kacperfaber/quickchart/ChartService.kt
@@ -0,0 +1,21 @@
+package io.github.kacperfaber.quickchart
+
+import org.springframework.stereotype.Service
+
+@Service
+class ChartService(var api: ChartApi, var payloadGenerator: PayloadGenerator) {
+    fun createDoughnut(map: HashMap<String, Any>): ChartUrl? {
+        val payload = payloadGenerator.generate(ChartType.Doughnut, map.map { x -> x.key }, listOf(map.map { x -> x.value }))
+        return api.create(payload).toNullableUrl()
+    }
+
+    fun createDoughnut(labels: List<String>, dataSet: List<Any>): ChartUrl?{
+        val payload = payloadGenerator.generate(ChartType.Doughnut, labels, listOf(dataSet))
+        return api.create(payload).toNullableUrl()
+    }
+
+    fun create(chartType: ChartType, labels: List<String>, dataSets: List<List<Any>>): ChartUrl? {
+        val payload = payloadGenerator.generate(chartType, labels, dataSets)
+        return api.create(payload).toNullableUrl()
+    }
+}
