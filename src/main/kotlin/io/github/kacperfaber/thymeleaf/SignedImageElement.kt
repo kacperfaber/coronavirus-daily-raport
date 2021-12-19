32125bbcff070e13f2e7800a0ed46e4ba38c82d1
Kacper Faber <kacperf1234@gmail.com>
2021-12-19 16:44:09 +0100

    Modified SignedImageElement.kt to overrides EmptyHtmlElement instead HtmlElement

--- src/main/kotlin/io/github/kacperfaber/thymeleaf/SignedImageElement.kt
+++ src/main/kotlin/io/github/kacperfaber/thymeleaf/SignedImageElement.kt
@@ -1,8 +1,8 @@
 package io.github.kacperfaber.thymeleaf
 
-import io.github.kacperfaber.html.HtmlElement
+import io.github.kacperfaber.html.EmptyHtmlElement
 
-class SignedImageElement(var title: String, var url: String, var width: Int, var height: Int) : HtmlElement("img","") {
+class SignedImageElement(var title: String, var url: String, var width: Int, var height: Int) : EmptyHtmlElement() {
     override fun render(): String {
         return "<h3>${title}</h3><img src=\"${url}\" width=\"${width}\" height=\"${height}\"/>"
     }
