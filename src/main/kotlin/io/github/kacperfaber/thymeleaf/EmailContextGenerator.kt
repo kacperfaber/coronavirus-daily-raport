972264c720d7d6d8352c7d69d9dd0e9815482b86
Kacper Faber <kacperf1234@gmail.com>
2021-12-20 10:17:42 +0100

    DefaultEmailContextGenerator didn't implement interface.

--- src/main/kotlin/io/github/kacperfaber/thymeleaf/EmailContextGenerator.kt
+++ src/main/kotlin/io/github/kacperfaber/thymeleaf/EmailContextGenerator.kt
@@ -2,7 +2,9 @@ package io.github.kacperfaber.thymeleaf
 
 import io.github.kacperfaber.api.Subscription
 import org.thymeleaf.context.Context
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 interface EmailContextGenerator {
-    fun generate(subscription: Subscription, html: String): Context
+    fun generate(subscription: Subscription, html: String, generatedAt: LocalDateTime, reportDate: LocalDate): Context
 }
