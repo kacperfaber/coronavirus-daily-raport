a07e09d903382d849db45d3f765cab20feaa9215
Kacper Faber <kacperf1234@gmail.com>
2021-12-22 14:54:49 +0100

    Modified AverageAnalyzerExtensions.kt

--- src/main/kotlin/io/github/kacperfaber/analyzer/AverageAnalyzerExtensions.kt
+++ src/main/kotlin/io/github/kacperfaber/analyzer/AverageAnalyzerExtensions.kt
@@ -3,6 +3,18 @@ package io.github.kacperfaber.analyzer
 import io.github.kacperfaber.grouping.MonthGroup
 import java.time.LocalDate
 
+fun <TKey, TVal> List<MonthGroup<Int>>.average(key: (LocalDate) -> TKey, value: (Double) -> TVal): HashMap<TKey, TVal> {
+    val res = hashMapOf<TKey, TVal>()
+    for (m in this) {
+        var total = 0
+        for (item in m.items) {
+            total += item.value
+        }
+        res[key(m.date)] = value((total / m.items.count()).toDouble())
+    }
+    return res
+}
+
 fun List<MonthGroup<Int>>.average(): HashMap<LocalDate, Double> {
     val res = hashMapOf<LocalDate, Double>()
     for (m in this) {
